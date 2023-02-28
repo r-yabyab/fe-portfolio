9dbbe29157f50e961ecdf01f9e1157841daebd46
Scruffy pc <cayabyabrr@yahoo.com>
2023-02-28 00:27:10 -0800

    photos etc

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import RainbowDarkness from '../photos/3bf40ef49cba4e57b04b9319472d3d32.png'
 import TestPFP from '../photos/aboutmecropped111.png'
+import Linkedin from '../photos/linked.jpg'
 import CloudStock from '../photos/0adfa6c3203466b17c21befa04d7a3ee.png'
 import Clay from '../photos/IMG_7622.jpg'
 
@@ -15,40 +16,46 @@ function HomePage () {
 
     return (
         <>
-        {/* mainContainer */}
-        <div className="relative pl-10 pr-10 m-auto max-w-[1400px]  ">
+            {/* mainContainer */}
+            {/* <div className="relative pl-10 pr-10 m-auto max-w-[1400px] "> */}
+            <div className="relative m-auto max-w-[1400px] bg-neutral-200 ">
 
-            <div className="text-center pt-10">
-                <img draggable="false" className=" select-none w-[200px] h-[200px] object-cover rounded-full m-auto mb-10 " src={TestPFP} alt="me" />
-                <div className="text-xl">About Me</div>
-                <div>Hello! My name is Roderick, I'm an aspiring web developer from Hayward, California.
-                    I'm fascinated with creating practical webapps for everyday use. Don't believe me? Look at my projects!!!!!
-                    Having a background in psychology, I was interested in ways I can incorporate
-                    mental health to simple webpages that is accessible to everyone. I found it unfair that mental
-                    health apps today, and tracking apps in general, requires overhead such as registering with emails
-                    and answering questions before getting to the actual content. I applied this to other websites I wanted to simplify.
-                    <p>My goal? Practical webapps with simple & intuitive UI :)</p>
+                <div className="text-center pt-10 pl-10 pr-10 ">
+                    <img draggable="false" className=" select-none w-[200px] h-[200px] object-cover rounded-full m-auto mb-10 " src={Linkedin} alt="me" />
+                    <div className="text-xl">About Me</div>
+                    <div>Hello! My name is Roderick, I'm an independent web developer
+                        who's fascinated in creating practical webapps for everyday use. As someone who experiences
+                        the pain of having to register everytime to use websites, I find enjoyment in creating websites with minimal overhead for users.
+                        <br/><br/>Don't believe me? Check out my projects!!!!!
+                        <br/><br/>Having a background in psychology, I was interested in ways I can incorporate
+                        mental health to simple webpages that is accessible to everyone. I found it unfair that mental
+                        health apps today, and tracking apps in general, requires overhead such as registering with emails
+                        and answering questions before getting to the actual content. I applied this to other websites I wanted to simplify.
+                        <p>My goal? Practical webapps with simple & intuitive UI :)</p>
                     </div>
                 </div>
 
-                <div className="relative pt-10">
+                <div className="relative bg-white mt-20">
 
-                    <div className="text-center text-xl pb-10">Projects</div>
+                    <div className="text-center text-xl bg-white pt-8 pb-8 font-bold">Projects</div>
 
-{/* RainbowDarkness */}
-<div className="flex space-x-[100px] justify-evenly">
-                    <div className="relative [&>div]:max-w-[500px]">
-                        <div className="relative w-[80%] max-w-[500px] text-xl font-bold
+                    {/* RainbowDarkness */}
+                    <div className="lg:flex lg:space-x-[100px] lg:justify-evenly 
+                    max-lg:flex-col max-lg:space-y-[100px] max-lg:text-center 
+                    [&>div>div]:m-auto  min-w-[100%] align-middle">
+                        {/* <div className="absolute bg-neutral-200 w-screen h-full" /> */}
+                        <div className="relative [&>div]:max-w-[500px] ">
+                            <div className="relative w-[80%] max-w-[500px] text-xl font-bold
                         hover:opacity-70 hover:cursor-pointer [&>div]:hover:opacity-100 select-none
                         ">
-                            <a target="_blank" href="https://rainbowdarkness.com/"><img className="" src={RainbowDarkness} alt="Rainbow Darkness.png" /></a>
+                            <a target="_blank" rel="noreferrer" href="https://rainbowdarkness.com/"><img className="" src={RainbowDarkness} alt="Rainbow Darkness.png" /></a>
                             <div className="
                             absolute top-[50%] -translate-y-1/2 right-[50%] translate-x-1/2 opacity-0 
-                            
+                             pointer-events-none
                             ">
                                 Go to live site!</div>
                         </div>
-                        <div className="italic font-semibold">Rainbow Darkness</div>
+                        <div className="italic pt-8 font-semibold">Rainbow Darkness</div>
                         <div className="font-semibold">React | Express | Node | MongoDB | Vercel</div>
                         <div className="">Happiness tracking application. Users can compare their scores with the entire userbase. </div>
                         <div className={boolState ? "text-yellow-400 select-none hover:text-slate-800 hover:cursor-pointer" :
@@ -64,22 +71,23 @@ function HomePage () {
                         <div className="relative w-[80%] max-w-[500px] text-xl font-bold
                         hover:opacity-70 hover:cursor-pointer [&>div]:hover:opacity-100 select-none
                         ">
-                            <a target="_blank" href="https://stockshapes-client.vercel.app/"><img className="" src={CloudStock} alt="Rainbow Darkness.png" /></a>
+                            <a target="_blank" rel="noreferrer" href="https://stockshapes-client.vercel.app/"><img className="" src={CloudStock} alt="Rainbow Darkness.png" /></a>
                             <div className="
                             absolute top-[50%] -translate-y-1/2 right-[50%] translate-x-1/2 opacity-0 
-                            
+                             pointer-events-none
                             ">
                                 Go to live site!</div>
                         </div>
-                        <div className="italic font-semibold">Stock Shapes</div>
+                        <div className="italic pt-8 font-semibold">Stock Shapes</div>
                         <div className="font-semibold">React | Express | Node | Vercel | EC2</div>
-                        <div className="">Real-time stock & future updates with draggable + resizeable components. Resize the webpage to aid your trading sessions!</div>
+                        {/* <div className="">Real-time stock & future updates with draggable + resizeable components. Resize the webpage to aid your trading sessions!</div> */}
+                        <div className="">Real-time stock prices with draggable + resizeable components. Resize the webpage to aid your trading sessions!</div>
                         <div className={boolState ? "text-yellow-400 select-none hover:text-slate-800 hover:cursor-pointer" :
                         "text-slate-400 select-none hover:text-slate-800 hover:cursor-pointer"}
                         onClick={boolHandler}
                         >Details</div>
                             <div onClick={boolHandler} className={boolState ? "bg-slate-200 hover:cursor-pointer" : "hidden"}>
-                            iex Apperate API, focus on API limiting
+                            Pulls data IEX Cloud API, uses SSE to connect to live prices, serverside deployed to EC2 as a way to accomodate live events that serverless can't support natively. $50 a month for my API costs, one session from market open to close tends to run around $1-4 per user that displays 5 stocks. Currently working on optimizing API calls to minimize API costs.
                             </div>
                     </div>
                     </div>
@@ -87,12 +95,13 @@ function HomePage () {
                 </div>
 
                 <div className="pt-10 mb-24">
-    <div className="text-center">______________________________________________________</div>
+    {/* <div className="text-center">______________________________________________________</div> */}
     <div className="font-bold">Education</div>
     <div>B.A. Psychology 2018-2021</div>
 
     <div className="pt-4 font-bold">Hobbies</div>
-    <div>Oil Painting</div>
+    <div>Roaming modern art galleries</div>
+    <img draggable='false' className="w-[200px] h-[200px] object-cover rounded-full select-none" src={TestPFP} alt="cat" />
     <div>Playing with my cat</div>
     <img draggable='false' className="w-[200px] h-[200px] object-cover rounded-full select-none" src={Clay} alt="cat" />
 </div>
--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -4,9 +4,9 @@ function TopNav () {
     return (
         <>
         
-        <div className="relative bg-black text-white pt-3 pb-12 text-2xl">
-            <div className="absolute">Roderick Cayabyab</div>
-            <div className="absolute left-[50%] -translate-x-1/2">WebDev Portfolio</div>
+        <div className="relative bg-neutral-200 text-black pt-3 pb-12 text-2xl">
+            <div className="absolute font-bold">Roderick Cayabyab</div>
+            <div className="absolute font-bold left-[50%] -translate-x-1/2">Web Developer</div>
         </div>
 
         </>
